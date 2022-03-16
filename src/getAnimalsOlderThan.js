4bd8a659b8d8bd413bda720b98cb7f881c4305ef
Raquel Santos <raquelsantossilva2405@outlook.com>
2022-03-16 19:23:34 -0300

    Adicionando novamente arquivos

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+    const animals = data.species
+        .filter((elements) => elements.name === animal)
+        .map((eleme) => eleme.residents)
+        .find((banana) => banana)
+        .every((bolo) => bolo.age > age);
+    return animals;
 }
+
+console.log(getAnimalsOlderThan('penguins', 10));
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+    if (employeeName === undefined) {
+        return {};
+    };
+    const employee = data.employees.find((element) => element.firstName === employeeName || element.lastName === employeeName);
+    return employee;
 }
 
+console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
