558ea107552af60910c06051c51be38b3ce4a49f
Raquel Santos <raquelsantossilva2405@outlook.com>
2022-03-16 21:41:05 -0300

    Adicionando primeira parte requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,8 +1,13 @@
 const data = require('../data/zoo_data');
 
+const idStephanie = '9e7d4524-363c-416a-8759-8aa7e50c0992'
+
 function isManager(id) {
-  // seu código aqui
+const manager = data.employees.filter((element) => element.id === id)
+.some((elementsome) => elementsome.managers[0] === idStephanie || elementsome.managers[0] === undefined)
+return manager
 }
+console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'))
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
