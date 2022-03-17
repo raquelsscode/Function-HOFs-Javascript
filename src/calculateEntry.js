1a5a343947948c1cc412d6d5d9f49ddd0b975f3f
Raquel Santos <raquelsantossilva2405@outlook.com>
2022-03-17 19:35:18 -0300

    Adicionando parte do requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,48 @@
 const data = require('../data/zoo_data');
 
+const infos = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
 function countEntrants(entrants) {
-  // seu código aqui
+  if (Object.keys(entrants) === 0) {
+    return true;
+  }
+  if (entrants === undefined) {
+    return 0;
+  }
+  const crianca = entrants.filter((item) => item.age < 18);
+  const adulto = entrants.filter((item) => item.age >= 18 && item.age < 50);
+  const idoso = entrants.filter((item) => item.age >= 50);
+  const object = {
+    adult: adulto.length,
+    child: crianca.length,
+    senior: idoso.length,
+  };
+  return object;
 }
+console.log(countEntrants({}));
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  const teste = entrants.map((element) => element.age);
+  const teste2 = teste.map((element) => {
+    if (element >= 18 && element < 50) {
+      return data.prices.adult;
+    } if (element >= 50) {
+      return data.prices.senior;
+    } if (element <= 5) {
+      return data.prices.child;
+    }
+    return element;
+  });
+  const sum = teste2.reduce((acc, curr) => acc + curr, 0);
+  return sum;
 }
+
+console.log(calculateEntry([{ name: 'Carlos Nogueira', age: 50 }]));
 
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+// const count = data.species.find((item) => item.name === animal.specie).residents;
+// // console.log(count);
 }
-
+countAnimals();
+console.log(data.species.map((item1) => item1.residents));
 module.exports = countAnimals;
