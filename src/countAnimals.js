bf3c05f13010eee70abb1422950463dfae1094d6
Raquel Santos <raquelsantossilva2405@outlook.com>
2022-03-18 11:53:28 -0300

    Adicionado requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,9 +1,39 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-// const count = data.species.find((item) => item.name === animal.specie).residents;
-// // console.log(count);
+function emptyAnimals() {
+  const newAnimalList = data.species.map((thisAnimal) => ({
+    name: thisAnimal.name,
+    residents: thisAnimal.residents,
+  }));
+  return newAnimalList.reduce((acc, curr) => {
+    acc[curr.name] = curr.residents.length;
+    return acc;
+  }, {});
 }
-countAnimals();
-console.log(data.species.map((item1) => item1.residents));
+emptyAnimals();
+
+function numberForAnimals(animal) {
+  const animals = data.species.find((item) => item.name === animal.specie).residents;
+  const count = animals.length;
+  return count;
+}
+numberForAnimals({ specie: 'giraffes' });
+
+function numberSexAndSpecies(animal) {
+  const animalssex = data.species.find((item) => item.name === animal.specie).residents;
+  const banana = animalssex.filter((item1) => item1.sex === animal.sex).length;
+  return banana;
+}
+(numberSexAndSpecies({ specie: 'elephants', sex: 'male' }));
+
+function countAnimals(animal) {
+  if (!animal) {
+    return emptyAnimals();
+  } if (Object.keys(animal).length === 2) {
+    return numberSexAndSpecies(animal);
+  }
+  return numberForAnimals(animal);
+}
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
+
 module.exports = countAnimals;
